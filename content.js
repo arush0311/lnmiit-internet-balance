569b72e7b32cb37f63595cb6c44daa8518e885c1
Arush Goyal <arushgyl@gmail.com>
2016-01-12 22:14:54 +0530

    initial version

---
+++ content.js
@@ -0,0 +1,61 @@
+// content.js
+
+var form = document.forms['frmHTTPClientLogin'];
+form.addEventListener('submit',function(evt){
+	evt.preventDefault();
+	username = form['username'].value;
+	password = form['password'].value;
+
+	if(typeof timeouts !== 'undefined')
+	{
+		for (var i=0; i<timeouts.length; i++) {
+			clearTimeout(timeouts[i]);
+		}
+	}
+
+	var body = document.getElementsByTagName('body')[0];
+	body.style.width = '70%';
+
+	var html = document.getElementsByTagName('html')[0];
+	html.style.display = 'flex';
+
+	if(!(iUsage = document.getElementById('iUsage')))
+	{
+		iUsage = document.createElement('div');
+		iUsage.setAttribute('id','iUsage')
+		iUsage.style.width = '30%';
+		iUsage.style.padding = '50px'
+		iUsage.style.color = "#336699";
+		html.appendChild(iUsage);
+	}
+
+	timeouts = [];
+	function getBalance() {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET','https://172.22.2.2/corporate/webpages/myaccount/AccountStatus.jsp?popup=0');
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				xml = xhr.responseText;
+				var x = document.createElement('div');
+				x.innerHTML = xml;
+				var tds = x.getElementsByTagName('td');
+				var totalData = parseFloat(tds[60].innerHTML);
+				var remainingData = parseFloat(tds[64].innerHTML);
+
+				iUsage.innerHTML = "<h3>Total Daily Data: "+ totalData+" MB </h3><h3>Remaining Data: "+ remainingData+" MB </h3>";
+			}
+		}
+		xhr.send();
+		timeouts.push(setTimeout(getBalance,10000));
+	}
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST','https://172.22.2.2/corporate/Controller');
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			getBalance();
+		}
+	};
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send("mode=451&json=%7B%22username%22%3A%22"+ username +"%22%2C%22password%22%3A%22"+ password +"%22%2C%22languageid%22%3A%221%22%2C%22browser%22%3A%22Chrome_47%22%7D&__RequestType=ajax");
+});
---
+++ readme.md~
@@ -0,0 +1,15 @@
+<h2>Internet Usage of LNMIIT</h2>
+<h3>Chrome Extension</h3><hr>
+
+<ul>
+	<li>Download the extension from <p>dist</p> folder</li>
+	<li>Go to the <a href="chrome://extentions">chrome://extentions</a> page</li>
+	<li>Drag and drop the .crx file to the page</li>
+</ul>
+
+<br><br>
+<b>For Suggestions to improve mail me at arushgyl@gmail.com</b>
+<br><br>
+
+<b>Cheers!</b>
+
